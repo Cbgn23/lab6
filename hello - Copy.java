3bdbcd6ba9e9cd262e35dcc3aaeb5672f8a24f69
Manveen Singh <162100599+Cbgn23@users.noreply.github.com>
2025-03-20 14:46:38 +0000

    ma

--- hello - Copy.java
+++ hello - Copy.java
@@ -1,5 +1,1 @@
-class main{
-	public static void main(String [] args){
-		System.out.println("hello world");
-	}
-}
+"class main{public static void main(String [] args){System.out.println("hoi");}}" 
